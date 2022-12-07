97d024074ab18eb1a050b3925e346f8b27b7e5da
Andi365 <Anders1998@live.dk>
2022-12-07 19:39:43 +0100

    update to cart input fields

--- src/interactive-card-details-form/cardInputFields.tsx
+++ src/interactive-card-details-form/cardInputFields.tsx
@@ -1,26 +1,29 @@
 import React from 'react'
 
-type Props = {
+interface CardSimpleInputProps {
     labelText: string,
     placeholder: string,
-    name: string
+    name: string,
+    value: string,
+    classList: string,
+    handleOnChange: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const CardSimpleInput = (props: Props) => {
+const CardSimpleInput = ({ labelText, placeholder, name, value, classList, handleOnChange }:CardSimpleInputProps) => {
   return (
     <div className='pt-4'>
-      <label htmlFor={props.name} className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>{props.labelText}</label>
-      <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name={props.name} type='text' value='' placeholder={props.placeholder} />
+      <label htmlFor={name} className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>{labelText}</label>
+      <input className={classList} name={name} type='text' value={value} placeholder={placeholder} onChange={handleOnChange} />
     </div>
   )
 }
 
-const CardExpieryInput = (props: Props) => {
-  <div className='pt-4'>
-    <label htmlFor={props.name} className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>{props.labelText}</label>
-    <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name={props.name} type='text' value='' placeholder={props.placeholder} />
-    <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name={props.name} type='text' value='' placeholder={props.placeholder} />
-  </div>
-}
+// const CardExpieryInput = (props: Props) => {
+//   <div className='pt-4'>
+//     <label htmlFor={props.name} className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>{props.labelText}</label>
+//     <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name={props.name} type='text' value='' placeholder={props.placeholder} />
+//     <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name={props.name} type='text' value='' placeholder={props.placeholder} />
+//   </div>
+// }
 
 export default CardSimpleInput
--- src/interactive-card-details-form/carddetailscomponent.tsx
+++ src/interactive-card-details-form/carddetailscomponent.tsx
@@ -1,34 +1,91 @@
-import React from 'react'
+import React, { useReducer, useState } from 'react'
 import "@fontsource/space-grotesk/500.css"
 import CardSimpleInput from './cardInputFields'
+import './cardDetails.css'
 
-type Props = {
-
+interface CardDetails {
+  holderName: string,
+  cardNumber: string,
+  expMonth: string,
+  expYear: string,
+  cvc: string
 }
 
-const Carddetailscomponent = (props: Props) => {
+const initialState: CardDetails = {
+  holderName: '',
+  cardNumber: '',
+  expMonth: '',
+  expYear: '',
+  cvc: ''
+}
+
+
+
+const Carddetailscomponent = () => {
+  const [cardDetails, setCardDetails] = useState<CardDetails>(initialState)
+
+  const {holderName, cardNumber, expMonth, expYear, cvc } = cardDetails;
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value, name } = (e.target as HTMLInputElement);
+
+    switch (name) {
+      case 'holderName':
+        const isOnlyText = (/^[a-z A-Z]+$/).test(value)
+        console.log(isOnlyText);
+        
+        var regex = /^[a-zA-Z]+$/;
+        break;
+      case 'cardNumber':
+        break;
+    }
+    
+    setCardDetails({...cardDetails, [name]: value })
+
+  }
+
   return (
     <>
       <form className='mb-0 font-grotesk bg-white h-full px-4 pt-4 md:max-w-xs' action="#">
-        <CardSimpleInput labelText="Cardholder name" placeholder="e.g. Jane Appleseed" name='cardholder-name'/>
-        <CardSimpleInput labelText="Card number" placeholder="e.g. 1234 5678 9123 0000" name='card-number'/>
+        <CardSimpleInput labelText="Cardholder name" placeholder="e.g. Jane Appleseed" name='holderName' value={holderName}  handleOnChange={handleOnChange} classList='input-primary px-4 w-full'/>
+        <CardSimpleInput labelText="Card number" placeholder="e.g. 1234 5678 9123 0000" name='cardNumber' value={cardNumber} handleOnChange={handleOnChange} classList='input-primary px-4 w-full'/>
         <div className='pt-4 grid grid-cols-2 gap-2'>
           <div className='flex flex-col'>
-            <label htmlFor='exp-date' className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>exp. date (MM/YY)</label>
+            <label htmlFor='exp-date' className='label-primary'>exp. date (MM/YY)</label>
             <div className='flex gap-2'>
-              <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name='exp-date' type='text' value='' placeholder='MM' />
-              <input className="px-4 rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet w-full" name='exp-date' type='text' value='' placeholder='YY' />
+              <input className="input-primary px-4 w-full" name='expMonth' type='text' value={cardDetails.expMonth} placeholder='MM' onChange={handleOnChange} />
+              <input className="input-primary px-4 w-full" name='expYear' type='text' value={cardDetails.expYear} placeholder='YY' onChange={handleOnChange} />
             </div>
           </div>
           <div className='flex flex-col'>
-            <label htmlFor="cvc" className='inline-block mb-1 uppercase text-very-dark-violet text-sm font-medium'>CVC</label>
-            <input className='rounded-lg border-gray-300 shadow-sm placeholder:text-gray-300 focus:ring-light-grayish-violet' type="text" name="cvc" id="" placeholder='CVC' />
+            <label htmlFor="cvc" className='label-primary'>CVC</label>
+            <input className='input-primary' type="text" name="cvc" value={cvc} placeholder='CVC' onChange={handleOnChange}/>
           </div>
         </div>
-        <button className='w-full my-4 py-3 text-xl bg-very-dark-violet' type="submit">Confirm</button>
+        {/* <button className='w-full my-4 py-3 text-xl bg-very-dark-violet' type="submit">Confirm</button> */}
       </form>
     </>
   )
+
+   // function handleChange(evt: any) {
+    //   setState({ firstName: evt.target.value });
+    // }
+
+    // const [state, setState] = React.useState({
+    //     firstName: ""
+    //   })
+    //   return (
+    //     <form>
+    //       <label>
+    //         First name
+    //         <input className='text-black'
+    //           type="text"
+    //           value={state.firstName}
+    //           onChange={handleChange}
+    //         />
+    //       </label>
+    //     </form>
+    //   );
 }
 
 export default Carddetailscomponent
